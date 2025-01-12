bf190904b6bb84644671fc3f08e1da788765f1a9
Orenhadad12 <131456281+Orenhadad12@users.noreply.github.com>
2025-01-13 00:03:57 +0200

    soldierIndex

--- app/src/main/java/com/example/backgammon/AppConsts.java
+++ app/src/main/java/com/example/backgammon/AppConsts.java
@@ -4,6 +4,5 @@ public class AppConsts {
 
     public static final int MAX_SOLDIERS_IN_COL =4;
     public static final float DISTANCE_BETWEEN_SOLDIERS = 2.2f;
-
     public static float radius=0.0f;
 }
--- app/src/main/java/com/example/backgammon/BoardView.java
+++ app/src/main/java/com/example/backgammon/BoardView.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -21,167 +22,131 @@ public class BoardView extends View {
 
         board = new Board();
         gameManager = new GameManager(this, context,board);
-
-
     }
-
-
-    // here ew draw the board
-    // background - image
 
     private float[] positionArrayX = new float[24];
     private float[] positionArrayY = new float[24];
-    private float deltaY;
-  //  private float radius;
+    private float Canvas_size_Y = 0;
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
 
-        createPositionArrayY(canvas); // עשינו
+        createPositionArrayY(canvas);
         createPositionArrayX(canvas);
         drawBackground(canvas);
         drawWhite(canvas);
-        // draw the soldioes
-        //createSoldier(canvas,3,-1,5 );
-
-
         drawSoldiers(canvas);
-
     }
-
-
 
     public void createPositionArrayX(Canvas canvas) {
-
-        float shoreX = canvas.getWidth()/13;
-        float deltax = canvas.getWidth()/10 + canvas.getWidth()/22-shoreX;
-        float centralShoreX = canvas.getWidth()/8 + canvas.getWidth()/25;
-        for (int i = 0; i< positionArrayX.length/2; i++){
-            if (i< 6){
-                positionArrayX[11-i] = shoreX + i*deltax;
-                positionArrayX[12+i] = shoreX + i*deltax;
-            }
-            else {
-                positionArrayX[11-i] = centralShoreX + i*deltax;
-                positionArrayX[12+i] = centralShoreX + i*deltax;
+        float shoreX = canvas.getWidth() / 13;
+        float deltax = canvas.getWidth() / 10 + canvas.getWidth() / 22 - shoreX;
+        float centralShoreX = canvas.getWidth() / 8 + canvas.getWidth() / 25;
+        for (int i = 0; i < positionArrayX.length / 2; i++) {
+            if (i < 6) {
+                positionArrayX[11 - i] = shoreX + i * deltax;
+                positionArrayX[12 + i] = shoreX + i * deltax;
+            } else {
+                positionArrayX[11 - i] = centralShoreX + i * deltax;
+                positionArrayX[12 + i] = centralShoreX + i * deltax;
             }
         }
+
     }
-    public void createPositionArrayY(Canvas canvas){
-        float shoreY =canvas.getHeight()/10;
-        float deltay = canvas.getHeight()/11;
+
+    public void createPositionArrayY(Canvas canvas) {
+        float shoreY = canvas.getHeight() / 10;
+        float deltay = canvas.getHeight() / 11;
         positionArrayY[0] = shoreY;
-        positionArrayY[1] = canvas.getHeight()/10;
-        for (int i = 0; i< positionArrayY.length/2; i++){
+        positionArrayY[1] = canvas.getHeight() / 10;
+        for (int i = 0; i < positionArrayY.length / 2; i++) {
             positionArrayY[i] = shoreY;
-            positionArrayY[i+12] = canvas.getHeight() - shoreY;
+            positionArrayY[i + 12] = canvas.getHeight() - shoreY;
         }
-        radius = canvas.getHeight()/25;
+        radius = canvas.getHeight() / 25;
+        Canvas_size_Y = canvas.getHeight()/2;
     }
-
 
     private void drawWhite(Canvas canvas) {
-        // map the location white, eaten white to  visual locations
-
         Paint paint = new Paint();
-        paint.setColor(Color.WHITE);  // צבע העיגול
+        paint.setColor(Color.WHITE);
         paint.setStyle(Paint.Style.FILL);
-       // this.x = x;
-      //  this.y = y;
-
-
-
-
-
     }
 
     private void drawBackground(Canvas canvas) {
-        // background
         Drawable d = getResources().getDrawable(R.drawable.back, null);
-
         d.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-
-
         d.draw(canvas);
     }
 
-
-
-
-
     private void drawSoldiers(Canvas canvas) {
-        // map the location white, eaten white to  visual locations
-        // map the location black, eaten black to  visual locations
-        for(int i = 0; i<board.getLocWhite().length; i++){
-            createSoldier(canvas,i,  Color.WHITE,Color.BLACK,board.getLocWhite()[23-i]);
+        for (int i = 0; i < board.getLocWhite().length; i++) {
+            createSoldier(canvas, i, Color.WHITE, Color.BLACK, board.getLocWhite()[23 - i]);
         }
-        for(int i = 0; i<board.getLocBlack().length; i++){
-            createSoldier(canvas,i,  Color.BLACK,Color.WHITE,board.getLocBlack()[i]);
+        for (int i = 0; i < board.getLocBlack().length; i++) {
+            createSoldier(canvas, i, Color.BLACK, Color.WHITE, board.getLocBlack()[i]);
         }
-
     }
 
-    public  void createSoldier(Canvas canvas, int index, int color,int borderColor,int count){
+    public void createSoldier(Canvas canvas, int index, int color, int borderColor, int count) {
         float j = 0;
-        for (int i = 0; i <count; i++){
-           /* if(i< MAX_SOLDIERS_IN_COL) {
-                Soldier soldier = new Soldier(getContext(), positionArrayX[index], positionArrayY[index] + i * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color,borderColor);
-                soldier.draw(canvas);
-            }
-            else{
-                Soldier soldier = new Soldier(getContext(), positionArrayX[index] + 20, positionArrayY[index] + j * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color,borderColor);
-                soldier.draw(canvas);
-                j++;
-            }
-            */
+        for (int i = 0; i < count; i++) {
             if (index < 12) {
-                if(i< MAX_SOLDIERS_IN_COL) {
+                if (i < MAX_SOLDIERS_IN_COL) {
                     Soldier soldier = new Soldier(getContext(), positionArrayX[index], positionArrayY[index] + i * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color, borderColor);
                     soldier.draw(canvas);
-                }
-                else{
-                    Soldier soldier = new Soldier(getContext(), positionArrayX[index] + 20, positionArrayY[index] + j * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color,borderColor);
+                } else {
+                    Soldier soldier = new Soldier(getContext(), positionArrayX[index] + 20, positionArrayY[index] + j * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color, borderColor);
                     soldier.draw(canvas);
                     j++;
                 }
-            }
-            else{
-                if(i< MAX_SOLDIERS_IN_COL) {
+            } else {
+                if (i < MAX_SOLDIERS_IN_COL) {
                     Soldier soldier = new Soldier(getContext(), positionArrayX[index], positionArrayY[index] - i * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color, borderColor);
                     soldier.draw(canvas);
-                }
-                else{
-                    Soldier soldier = new Soldier(getContext(), positionArrayX[index] + 20, positionArrayY[index] - j * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color,borderColor);
+                } else {
+                    Soldier soldier = new Soldier(getContext(), positionArrayX[index] + 20, positionArrayY[index] - j * radius * DISTANCE_BETWEEN_SOLDIERS, radius, color, borderColor);
                     soldier.draw(canvas);
                     j++;
                 }
             }
-
-
         }
     }
 
-
-
-
-    public  float[] getPositionArrayX() {
+    public float[] getPositionArrayX() {
         return positionArrayX;
     }
-    public  float[] getPositionArrayY() {
+
+    public float[] getPositionArrayY() {
         return positionArrayY;
     }
 
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            int soldierIndex = getSoldierTouch(event);
+            if (soldierIndex != -1) {
+                Toast.makeText(getContext(), "Clicked at: (" + soldierIndex + ")", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        }
         return super.onTouchEvent(event);
     }
 
-    public void NextTurnLight(float locx,float locy,int x1, int x2){
-    //לעשות תנאים למציאת המיקום והאינדס של האיבר עליו אנחנו לוחצים
-    //
+    private int getSoldierTouch(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        for (int i = 0; i < 12; i++) {
+            if (x > positionArrayX[i] - radius && x < positionArrayX[i] + radius && Canvas_size_Y / 2 > y) {
+                return i;
+            } else if (x > positionArrayX[i] - radius && x < positionArrayX[i] + radius) {
+                return 23 - i;
+            }
+        }
+        return -1; // Return -1 if no soldier is clicked
     }
 
-
+    public void NextTurnLight(float locx, float locy, int x1, int x2) {
+    }
 }
